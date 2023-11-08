08e54962702786501b0703216a418018c75f7fda
徐晨瑜 <11094724+xuchen-12@user.noreply.gitee.com>
2023-11-08 10:24:49 +0800

    Initial commit

---
+++ shop_online/src/main/java/com/soft/shop/constant/Constant.java
@@ -0,0 +1,7 @@
+package com.soft.shop.constant;
+
+public interface Constant {
+    String CREATE_TIME="creatTime";
+    String UPDATE_TIME="updateTime";
+    String DELETED_FLAG="deleteFlag";
+}
---
+++ shop_online/src/test/java/com/soft/shop/ShopOnlineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soft.shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopOnlineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
